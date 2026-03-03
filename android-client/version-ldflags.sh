2a0e03dbd694803f9f75ffbd2f7bea21d74f1338
Dan Staples <dan@acab.enterprises>
2026-03-03 14:24:11 +0000

    android-client: Switched from tailscale.version to manually-created barghest.version file, and Makefile target that creates it

--- android-client/version-ldflags.sh
+++ android-client/version-ldflags.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-source tailscale.version || { echo >&2 "no tailscale.version file found"; exit 1; }
+source barghest.version || { echo >&2 "no barghest.version file found"; exit 1; }
 if [[ -z "${VERSION_LONG}" ]]; then
     exit 1
 fi
 echo "-X tailscale.com/version.longStamp=${VERSION_LONG}"
 echo "-X tailscale.com/version.shortStamp=${VERSION_SHORT}"
 echo "-X tailscale.com/version.gitCommitStamp=${VERSION_GIT_HASH}"
-echo "-X tailscale.com/version.extraGitCommitStamp=${VERSION_EXTRA_HASH}"
